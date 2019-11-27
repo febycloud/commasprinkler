13efefcded85399c2fbdf5988be3350d1f756402
Fei <unshi@outlook.com>
2019-11-27 11:12:26 -0500

    t

--- algo.py
+++ algo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 
-str="please sit spot.sit spot,sit.spot here now here."
+str="please,sit spot.sit spot,sit.spot,here now,here"
 def sprinklecommas(str,precedes=[],follows=[]):
     sentences=re.split(r"\.",str)
     if len(sentences)<1:
@@ -12,13 +12,14 @@ def sprinklecommas(str,precedes=[],follows=[]):
             for i in range(len(phrases)):
                 phrase=phrases[i]
                 if i<range(len(phrases)-1):
-                    words=re.split(r"\s" ,phrase)
+                    words=re.split("\s" ,phrase)
                     if not any(words[-1]==p for p in follows):
                         follows.append(words[-1])
                 if i>0:
                     precedingwith=re.split("\s",phrase) 
-                    if precedingwith[0]!=None and not any(precedingwith[0]==p for p in precedes):
+                    if precedingwith[0]!='' and not any(precedingwith[0]==p for p in precedes):
                         precedes.append(precedingwith[0])
+    
     print precedingwith
     print follows
 
